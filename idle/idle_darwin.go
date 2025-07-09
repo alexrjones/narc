ea3445791628908f8b3fb888b76d8dbadd620ff2
alexrjones <alexander.jones@familyzone.com>
2025-07-09 09:40:51 +0800

    Try to prevent what seems to be spurious tracking

--- idle/idle_darwin.go
+++ idle/idle_darwin.go
@@ -14,13 +14,13 @@ const idleThresh = 300
 func IdleChan() <-chan narc.IdleState {
 
 	ch := make(chan narc.IdleState, 1)
-	var activeMu sync.Mutex
-	active := true
+	var awakeMu sync.Mutex
+	systemAwake := true
 	go func() {
 		StartSleepWatcher(func(b bool) {
-			activeMu.Lock()
-			active = b
-			activeMu.Unlock()
+			awakeMu.Lock()
+			systemAwake = b
+			awakeMu.Unlock()
 			if b {
 				ch <- narc.IdleState{Active: true, ChangeReason: narc.ChangeReasonSystemAwake}
 			} else {
@@ -28,22 +28,28 @@ func IdleChan() <-chan narc.IdleState {
 			}
 		})
 	}()
+	userActive := true
 	pollInterval := time.Second * 10
 	go func() {
 		for {
 			select {
 			case <-time.After(pollInterval):
 				{
+					awakeMu.Lock()
+					if !systemAwake {
+						awakeMu.Unlock()
+						return
+					}
+					awakeMu.Unlock()
 					idleSeconds := getIdleSeconds()
-					var oldState bool
-					activeMu.Lock()
-					oldState = active
-					active = idleSeconds < idleThresh
-					activeMu.Unlock()
-					if idleSeconds >= idleThresh {
-						ch <- narc.IdleState{Active: false, ChangeReason: narc.ChangeReasonUserIdle}
-					} else if oldState == false {
-						ch <- narc.IdleState{Active: true, ChangeReason: narc.ChangeReasonUserActive}
+					oldState := userActive
+					userActive = idleSeconds < idleThresh
+					if oldState != userActive {
+						if userActive {
+							ch <- narc.IdleState{Active: true, ChangeReason: narc.ChangeReasonUserActive}
+						} else {
+							ch <- narc.IdleState{Active: false, ChangeReason: narc.ChangeReasonUserIdle}
+						}
 					}
 				}
 			}
